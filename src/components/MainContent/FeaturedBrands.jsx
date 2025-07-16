63ac1f5fa7a568cf99d4f11efbec9a158d1b85d9
najijrwan <najijrwanyt@gmail.com>
2025-07-16 16:32:56 +0300

    .

---
+++ src/components/MainContent/FeaturedBrands.jsx
@@ -0,0 +1,46 @@
+export default function FeaturedBrands({ title }) {
+
+    return (
+        <section className="p-2 mx-2 bg-sections rounded-lg border border-classic">
+            <header className='flex items-center justify-between'>
+                <h2 className="text-2xl font-semibold text-base-1">{title}</h2>
+            </header>
+
+            <div className="group whitespace-nowrap mt-6 overflow-hidden">
+                <div className="flex brand-track animate-marquee space-x-8 w-max">
+                    <img src="/brands/addidas.jpg" alt="addidas" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/chanel.jpeg" alt="chanel" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/dior.JPG" alt="dior" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/gucci.png" alt="gucci" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/louis-vuitton.png" alt="louis-vuitton" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/nike.jpg" alt="nike" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/prada.png" alt="prada" className="h-30 cursor-pointer rounded-xl bg-white" />
+                    <img src="/brands/zara.jpg" alt="zara" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/apple.jpeg" alt="apple" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/lenovo.webp" alt="lenovo" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/oneplus.webp" alt="oneplus" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/oppo.avif" alt="oppo" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/realme.webp" alt="realme" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/samsung.jpg" alt="samsung" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/xiaomi.jpeg" alt="xiaomi" className="h-30 cursor-pointer rounded-xl bg-black" />
+
+                    <img src="/brands/addidas.jpg" alt="addidas" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/chanel.jpeg" alt="chanel" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/dior.JPG" alt="dior" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/gucci.png" alt="gucci" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/louis-vuitton.png" alt="louis-vuitton" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/nike.jpg" alt="nike" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/prada.png" alt="prada" className="h-30 cursor-pointer rounded-xl bg-white" />
+                    <img src="/brands/zara.jpg" alt="zara" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/apple.jpeg" alt="apple" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/lenovo.webp" alt="lenovo" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/oneplus.webp" alt="oneplus" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/oppo.avif" alt="oppo" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/realme.webp" alt="realme" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/samsung.jpg" alt="samsung" className="h-30 cursor-pointer rounded-xl bg-black" />
+                    <img src="/brands/xiaomi.jpeg" alt="xiaomi" className="h-30 cursor-pointer rounded-xl bg-black" />
+                </div>
+            </div>
+        </section >
+    );
+}
--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -3,7 +3,7 @@ import Hero from "./Hero";
 import NewArrivals from "./NewArrivals";
 import PromotionBanner from "./PromotionBanner";
 import TopSellings from "./TopSellings";
-import ScrollingMarquee from "./ScrollingMarquee";
+import FeaturedBrands from "./FeaturedBrands";
 import NewsletterSignup from "./NewsletterSignup";
 import HorizontalProductScroller from "./HorizontalProductScroller";
 import Testimonials from "./Testimonials";
@@ -15,7 +15,7 @@ export default function MainContent() {
             <NewArrivals title="New Arrivals" />
             <PromotionBanner />
             <TopSellings title="Top Sellings" />
-            <ScrollingMarquee title="Featured Brands"/>
+            <FeaturedBrands title="Featured Brands"/>
             <HorizontalProductScroller title="Recommended for You" />
             <Testimonials />
             <NewsletterSignup />
