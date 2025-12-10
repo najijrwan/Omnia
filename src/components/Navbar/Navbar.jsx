d8f8506ea9a042b25111535b10b38c9f50da4691
najijrwan <najijrwanyt@gmail.com>
2025-12-10 20:32:17 +0200

    ...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { categories } from "../../data/navBar/categories";
 import SearchBar from "./SearchBar";
 import Sidebar from "./Sidebar";
 import CategoriesWithDetails from './CategoriesWithDetails';
-import { ShoppingCart, CircleUserRound, Bookmark, Search } from 'lucide-react';
+import { Search, Bookmark, ShoppingCart, CircleUserRound, ArrowLeftFromLine } from 'lucide-react';
 
 import { Menu } from 'lucide-react';
 
@@ -17,9 +17,14 @@ export default function Navbar() {
   return (
     <>
       <header className="
-      relative bg-main py-3 px-2 2xl:px-40 flex flex-row justify-between items-center md:gap-5 text-white transition-all ease-in-out duration-500">
+      relative 
+      flex flex-row justify-between items-center lg:justify- 
+      py-3 px-2 xs:px-4 sm:px-6 md:px-8 lg:px-12 2xl:px-24! 
+      text-white bg-main 
+      transition-all ease-in-out duration-500"
+      >
 
-        <Link to="/" className="font-bold text-xl text-base-1 hover:opacity-70">
+        <Link to="/" className="font-bold text-lg text-base-1 hover:opacity-70">
           <i>Omnia</i>
         </Link>
 
@@ -29,32 +34,38 @@ export default function Navbar() {
           <button className="hidden xl:block font-bold text-xl text-base-1 hover:opacity-70 cursor-pointer"><span>Categories</span></button>
         </div> */}
 
-        <ul className="hidden lg:flex h-full absolute items-center gap-7 text-base-1 font-extrabold">
+        <ul className="hidden absolute left-1/2 -translate-x-1/2 lg:flex h-full items-center justify-center gap-7 text-base-1 font-extrabold">
           <li className="flex items-center cursor-pointer hover:border-b-base-1 hover:border-b-2 box-border"><span>About</span></li>
           <li className="flex items-center cursor-pointer hover:border-b-base-1 hover:border-b-2 box-border"><span>Support</span></li>
           <li className="flex items-center cursor-pointer hover:border-b-base-1 hover:border-b-2 box-border"><span>Follow Us</span></li>
           <li className="flex items-center cursor-pointer hover:border-b-base-1 hover:border-b-2 box-border"><span>Copmany</span></li>
         </ul>
 
-        <ul className="flex items-center justify-center gap-2 py-1.5 px-2.5 rounded-2xl backdrop-blur-3xl bg-base-1/20 border border-base-1/20 shadow-[0_4px_20px_rgba(0,0,0,0.1)]">
-          <li className="lg:hidden ">
-            <button className="py-2 px-3 size-fit rounded-xl bg-base-1/5 hover:bg-base-1/15 active:scale-90 transition-all duration-200 ease-in-out shadow-[0_0_3px_rgba(0,0,0,0.2)] cursor-pointer">
-              <Search className="size-6 text-base-1/90 drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]" />
+        <ul className="
+          flex items-center justify-center 
+          gap-2 py-1.5 px-2.5 
+          rounded-2xl backdrop-blur-3xl bg-base-1/20
+          border border-base-1/20 shadow-[0_4px_20px_rgba(0,0,0,0.1)]"
+        >
+          <li className="">
+            <SearchBar />
+            <button className="py-2 px-3 rounded-xl bg-base-1/5 hover:bg-base-1/15 active:scale-90 transition-all duration-200 ease-in-out shadow-[0_0_3px_rgba(0,0,0,0.2)] cursor-pointer">
+              <Search className="size-5 text-base-1/90 drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]" />
             </button>
           </li>
-          <li className="flex items-center">
+          <li className="">
             <button className="py-2 px-3 rounded-xl bg-base-1/5 hover:bg-base-1/15 active:scale-90 transition-all duration-200 ease-in-out shadow-[0_0_3px_rgba(0,0,0,0.2)] cursor-pointer">
-              <Bookmark className="size-6 text-base-1/90 drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]" />
+              <Bookmark className="size-5 text-base-1/90 drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]" />
             </button>
           </li>
-          <li className="flex items-center">
+          <li className="">
             <button className="py-2 px-3 rounded-xl bg-base-1/5 hover:bg-base-1/15 active:scale-90 transition-all duration-200 ease-in-out shadow-[0_0_3px_rgba(0,0,0,0.2)] cursor-pointer">
-              <ShoppingCart className="size-6 text-base-1/90 drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]" />
+              <ShoppingCart className="size-5 text-base-1/90 drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]" />
             </button>
           </li>
-          <li className="flex items-center">
+          <li className="">
             <button className="py-2 px-3 rounded-xl bg-base-1/5 hover:bg-base-1/15 active:scale-90 transition-all duration-200 ease-in-out shadow-[0_0_3px_rgba(0,0,0,0.2)] cursor-pointer">
-              <CircleUserRound className="size-6 text-base-1/90 drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]" />
+              <CircleUserRound className="size-5 text-base-1/90 drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]" />
             </button>
           </li>
         </ul>
@@ -63,14 +74,14 @@ export default function Navbar() {
           onClick={() => setMenuOpen(!menuOpen)}
           className="
             lg:hidden flex items-center justify-center
-            size-auto p-[13px] rounded-[16px] 
+            size-auto py-2.5 px-3.5 rounded-2xl 
             text-base-1 bg-base-1/20 border border-base-1/30
             backdrop-blur-md shadow-[0_2px_10px_rgba(0,0,0,0.15)]
             transition-all duration-200 ease-in-out
             hover:bg-base-1/20 hover:shadow-[0_0_12px_rgba(0,0,0,0.25)]
             active:scale-95 active:shadow-inner"
         >
-          <Menu className="size-6 transition-transform duration-300 ease-in-out" />
+          <Menu className="size-6 transition-transform duration-300 ease-in-out drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]" />
         </button>
       </header>
 
--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -2,25 +2,25 @@ import { Search } from 'lucide-react';
 export default function SearchBar() {
     return (
         <div className="
-  relative w-full h-[54px] md:max-w-[400px] mx-auto xl:mx-0 xl:w-[178px]
-  rounded-2xl backdrop-blur-md bg-base-1/20 border border-base-1/20
-  shadow-[0_4px_20px_rgba(0,0,0,0.1)] transition-all duration-200
-  hover:bg-base-1/15
-">
-            <Search
-                className="absolute top-1/2 left-3 -translate-y-1/2 size-5 text-base-1/80 drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]"
-                strokeWidth={2.5}
-            />
+            hidden relative gap-3 w-[140px] h-[40px] md:max-w-[400px] mx-auto xl:mx-0 xl:w-[178px]
+            rounded-full backdrop-blur-md bg-secondary/40 border border-base-1/20
+            shadow-[0_4px_20px_rgba(0,0,0,0.3)] transition-all duration-200
+            hover:bg-base-1/5"
+        >
+            <button className='h-full bg-secondary/10 text-base-1/80 px-2 rounded-full active:scale-90 transition-all duration-200 ease-in-out shadow-[0_0_3px_rgba(255,255,255,0.2)] cursor-pointer'>
+                <Search
+                    className="size-6 rounded-[inherit] drop-shadow-[0_0_3px_rgba(255,255,255,0.5)]"
+                    strokeWidth={2}
+                />
+            </button>
             <input
                 type="text"
                 placeholder="Search"
                 onClick={(e) => e.stopPropagation()}
                 className="
-      w-full h-[54px] pl-10 pr-3 rounded-[inherit]
-      bg-transparent text-base-1/90 placeholder-base-1/60
-      focus:outline-none focus:ring-2 focus:ring-base-1/30
-      focus:placeholder-transparent transition-all duration-200 ease-in-out
-    "
+                    size-full
+                    bg-transparent text-base-1/90 placeholder-base-1/60
+                    focus:outline-none focus:placeholder-transparent transition-all duration-200 ease-in-out"
             />
         </div>
 
